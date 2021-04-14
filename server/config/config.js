2e3a68c4ef88eb7b642458e3bc0018953707e29f
andreseguanipa1 <andreseguanipa1@gmail.com>
2021-04-14 17:51:30 -0430

    v1.1.1

--- server/config/config.js
+++ server/config/config.js
@@ -15,7 +15,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://andreseguanipa1:8TmuM8cgbYgkboVI@cafe.x6bjp.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
